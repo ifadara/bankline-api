291519093de66e7828a0713db07d5dbf13d0df16
Icaro Silva <dsicejay@gmail.com>
2022-05-03 22:31:38 -0300

    Publicação do projeto no git

---
+++ src/main/java/com/dio/santander/bakline/api/BaklineApiApplication.java
@@ -0,0 +1,13 @@
+package com.dio.santander.bakline.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaklineApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaklineApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dio/santander/bakline/api/BaklineApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.santander.bakline.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaklineApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
